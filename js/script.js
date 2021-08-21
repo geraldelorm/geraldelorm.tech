fd5849f8fd6ff979d69ae28d721e25baeec4e565
geraldelorm <geraldelorm14@gmail.com>
2021-08-21 20:04:21 +0000

    mode toggle make simple and concise

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 const icon = document.getElementById('icon');
 const body = document.getElementById('body');
-const lIcon = document.getElementsByClassName('l-icon');
-const dIcon = document.getElementsByClassName('d-icon');
 
-icon.addEventListener('click', (e) => {
+icon.addEventListener('click', () => {
   body.classList.toggle('dark');
 });
-
-if (body.classList.contains('dark')) {
-  dIcon.classList.toggle('off');
-} else if (body.classList.contains('light')) {
-  lIcon.classList.toggle('off');
-}
